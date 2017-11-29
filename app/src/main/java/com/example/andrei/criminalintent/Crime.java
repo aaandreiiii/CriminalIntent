50916cb7ac39fc48e8d9c0221ffbb9bc5514e40c
andrei <aaandreiiii@gmail.com>
2017-11-29 22:12:59 +0200

    первый комит

---
+++ app/src/main/java/com/example/andrei/criminalintent/Crime.java
@@ -0,0 +1,47 @@
+package com.example.andrei.criminalintent;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        // Генерирование уникального идентификатора
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getDate() {
+        String myString = DateFormat.getDateInstance(DateFormat.FULL).format(mDate);
+        return myString;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
